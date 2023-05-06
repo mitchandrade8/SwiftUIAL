de44b89761a18f78c7a557be59392ce14767fd9c
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-05-06 16:59:47 -0600

    Initial Commit

---
+++ SwiftUIAL/SwiftUIALApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIALApp.swift
+//  SwiftUIAL
+//
+//  Created by Mitch Andrade on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIALApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
