db4df3ea63f9b778948f13b633c8fe9a8879a70e
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-05-10 13:35:26 -0600

    Seen how to adjust the frame from a child view to adjust to the parent view using a geometry reader

--- SwiftUIAL/PreferenceKeyBootcamp/GeometryPreferenceKeyBootcamp.swift
+++ SwiftUIAL/PreferenceKeyBootcamp/GeometryPreferenceKeyBootcamp.swift
@@ -8,23 +8,51 @@
 import SwiftUI
 
 struct GeometryPreferenceKeyBootcamp: View {
+    
+    @State private var rectSize: CGSize = .zero
+    
     var body: some View {
         VStack {
             Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .frame(width: rectSize.width, height: rectSize.height)
                 .background(Color.blue)
             Spacer()
             HStack {
                 Rectangle()
-                Rectangle()
+                
+                GeometryReader { geo in
+                    Rectangle()
+                        .updateRectangleGeoSize(geo.size)
+                }
                 Rectangle()
             }
             .frame(height: 55)
         }
+        .onPreferenceChange(RectangleGeometrySizePreferenceKey.self, perform: { value in
+            self.rectSize = value
+        })
+        .padding()
     }
 }
 
 struct GeometryPreferenceKeyBootcamp_Previews: PreviewProvider {
     static var previews: some View {
         GeometryPreferenceKeyBootcamp()
+    }
+}
+
+extension View {
+    
+    func updateRectangleGeoSize(_ size: CGSize) -> some View {
+        preference(key: RectangleGeometrySizePreferenceKey.self, value: size)
+    }
+}
+
+struct RectangleGeometrySizePreferenceKey: PreferenceKey {
+    
+    static var defaultValue: CGSize = .zero
+    
+    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
+        value = nextValue()
     }
 }
---
+++ SwiftUIAL/PreferenceKeyBootcamp/ScrollViewOffsetPreferenceKeyBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  ScrollViewOffsetPreferenceKeyBootcamp.swift
+//  SwiftUIAL
+//
+//  Created by Mitch Andrade on 5/10/23.
+//
+
+import SwiftUI
+
+struct ScrollViewOffsetPreferenceKeyBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScrollViewOffsetPreferenceKeyBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollViewOffsetPreferenceKeyBootcamp()
+    }
+}
