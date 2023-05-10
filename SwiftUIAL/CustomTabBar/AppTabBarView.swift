53bd5fe23228dd50964954a5111dee29b2fc3344
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-05-10 14:59:56 -0600

    Beginning 1/4 with a custom TabBar

---
+++ SwiftUIAL/CustomTabBar/AppTabBarView.swift
@@ -0,0 +1,44 @@
+//
+//  AppTabBarView.swift
+//  SwiftUIAL
+//
+//  Created by Mitch Andrade on 5/10/23.
+//
+
+import SwiftUI
+
+// Generics
+// ViewBuilder
+// PreferenceKey
+// MatchedGeometryEffect
+
+struct AppTabBarView: View {
+    
+    @State private var selection: String = "home"
+    
+    var body: some View {
+        TabView(selection: $selection) {
+            Color.red
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            Color.purple
+                .tabItem {
+                    Image(systemName: "heart")
+                    Text("Favorites")
+                }
+            Color.blue
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("Profile")
+                }
+        }
+    }
+}
+
+struct AppTabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppTabBarView()
+    }
+}
---
+++ SwiftUIAL/CustomTabBar/CustomTabBarView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTabBarView.swift
+//  SwiftUIAL
+//
+//  Created by Mitch Andrade on 5/10/23.
+//
+
+import SwiftUI
+
+struct CustomTabBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomTabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomTabBarView()
+    }
+}
